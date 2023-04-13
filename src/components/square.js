044e2d882dd9a84b97923272c5bf0e9a1305919b
vishal kumar <117971824+vishal9934@users.noreply.github.com>
2023-04-14 01:44:47 +0530

    first commit

---
+++ src/components/square.js
@@ -0,0 +1,9 @@
+
+const Square=(props)=>{
+    return(
+        <div onClick={props.onClick} className="square">
+            <h5>{props.value}</h5>
+        </div>
+    )
+}
+export default Square;
